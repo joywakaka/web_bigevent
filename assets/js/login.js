7cd0ffd9e91b6b3ba7fd988ebe95f102e75531cb
yuli <624996569@qq.com>
2020-11-24 10:44:13 +0800

    更新2

--- assets/js/login.js
+++ assets/js/login.js
@@ -28,23 +28,52 @@ layui.form.verify({
         }
 
     })
-    //监听注册表单提交事件  提交之后就会传输数据字符串
-let baseUrl = "http://ajax.frontend.itheima.net"
+    // 登录跳转部分
+    // let baseUrl = "http://ajax.frontend.itheima.net";
+
+
+
 $('#regForm').on('submit', submitData);
+$('#form_login').on('submit', function(e) {
+        e.preventDefault();
+        let dataStr = $(this).serialize();
+        $.ajax({
+            url: '/api/login',
+            method: 'post',
+            data: dataStr,
+            success(res) {
+                if (res.status != 0) return layui.layer.msg(res.message);
+                layui.layer.msg(res.message, {
+                    icon: 6,
+                    time: 1500,
 
+                }, function() {
+                    // 将登录成功的token保存到本地存储
+                    localStorage.setItem('token', res.token);
+                    location.href = '/index.html'
+                })
+            }
+        })
+    })
+    //监听注册表单提交事件  提交之后就会传输数据字符串
 function submitData(e) {
-
     e.preventDefault();
     let datastr = $(this).serialize();
     $.ajax({
-        url: baseUrl + '/api/reguser',
+        url: '/api/reguser',
         method: 'post',
         data: datastr,
         success(res) {
             if (res.status != 0) return layui.layer.msg(res.message);
             // console.log(res.message);
             layui.layer.msg(res.message);
+            let uname = $('.reg-box [name=username]').val().trim();
+            $('.login-box [name=username').val(uname);
+            let upwd = $('.reg-box [name=password]').val().trim();
+            $('.login-box [name=password').val(upwd);
+            // 清空注册表单
             $('#regForm')[0].reset();
+            // 切换到登录idv
             $('#link_login').click();
         }
     })
