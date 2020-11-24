c1b4f2b69a9d4eb6573aa3b7c473749a0cc4e178
yuli <624996569@qq.com>
2020-11-24 19:00:47 +0800

    mster更新

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-=======
-// 基于jq的ajax请求发送之前 对数据进行预处理 
-$.ajaxPrefilter(function(ajaxOpt) {
-    // console.log(ajaxOpt);
-    ajaxOpt.url = 'http://ajax.frontend.itheima.net' + ajaxOpt.url
-
-
-    //统一为有权限的接口 设置headers 请求头
-    if (options.url.indexOf('/my/') > -1) {
-        options.headers = {
-            Authorization: localStorage.getItem('token')
-        }
-    }
+//在进行ajax 的异步请求时 会先调用ajaxPrefilter 这个函数 在这个函数中我们可以拿到我们给ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
 
 })
-
-//为所有的异步对象都同意挂载 complete 函数
-// 全局统一挂载 complete 回调函数
-options.complete = function(res) {
-    // console.log('执行了 complete 回调：')
-    // console.log(res)
-    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-        // 1. 强制清空 token
-        localStorage.removeItem('token')
-            // 2. 强制跳转到登录页面
-        location.href = '/login.html'
-    }
-}
->>>>>>> index
